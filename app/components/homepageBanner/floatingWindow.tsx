1347e5697ada4976cd7c387f842ddd78508f8cf7
Harsh Sahare <harshsahare1212@gmail.com>
2025-08-04 18:06:58 +0530

    feat : added homepage banner

---
+++ app/components/homepageBanner/floatingWindow.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import React from "react";
+
+function FloatingWindow() {
+  return (
+    <div className="absolute top-0 left-[50%] translate-[-50%] z-2 flex gap-4 w-fit p-5 justify-around items-center text-white bg-[#923C1E] rounded-br-[40px] rounded-tl-[40px]">
+      <div className="flex flex-col  items-center space-y-2">
+        <div className="h-[100px] flex items-center">
+          <Image
+            width={100}
+            height={100}
+            alt="ongoing programs"
+            src="/svg/homepageBannerImage3.svg"
+          />
+        </div>
+        <span className="font-cinzel text-sm uppercase tracking-widest">
+          Ongoing Programs
+        </span>
+      </div>
+      <div className="flex flex-col items-center space-y-2">
+        <div className="h-[100px] flex items-center">
+          <Image
+            width={60}
+            height={60}
+            alt="Spiritual Retreats"
+            src="/svg/homepageBannerImage2.svg"
+          />
+        </div>
+        <span className="font-cinzel text-sm uppercase tracking-widest">
+          Spiritual Retreats
+        </span>
+      </div>
+      <div className="flex flex-col items-center space-y-2">
+        <div className="h-[100px] flex items-center">
+          <Image
+            width={60}
+            height={60}
+            alt="Special Events"
+            src="/svg/homepageBannerImage1.svg"
+          />
+        </div>
+        <span className="font-cinzel text-sm uppercase tracking-widest">
+          Special Events
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default FloatingWindow;
