00a707d294de5a5fb377af698519aafe4d139a85
Harsh Sahare <harshsahare1212@gmail.com>
2025-08-05 17:52:23 +0530

    refactored : made the navbar fixed instead of sticky

--- app/components/layout/navbar.tsx
+++ app/components/layout/navbar.tsx
@@ -3,7 +3,7 @@ import Desktopnavbar from "./navbar/desktopnavbar";
 
 function Navbar() {
   return (
-    <nav className="sticky top-0 left-0 w-full flex justify-center py-5 z-50">
+    <nav className="fixed top-0 left-0 w-full flex justify-center py-5 z-50">
       <Desktopnavbar />
     </nav>
   );
