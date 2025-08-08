9c8871d7e8668b75d8b19c8ac29ec5fb49d08d5b
Harsh Sahare <harshsahare1212@gmail.com>
2025-08-08 22:24:03 +0530

    refactored : removed the pointer event of the outer div

--- app/components/layout/navbar.tsx
+++ app/components/layout/navbar.tsx
@@ -3,8 +3,10 @@ import Desktopnavbar from "./navbar/desktopnavbar";
 
 function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full flex justify-center py-5 z-50">
-      <Desktopnavbar />
+    <nav className="fixed top-0 left-0 w-full flex justify-center py-5 z-50 pointer-events-none ">
+      <div className="pointer-events-auto">
+        <Desktopnavbar />
+      </div>
     </nav>
   );
 }
