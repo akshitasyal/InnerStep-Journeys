84046bf8a1204424dc8dc9646177f3d540b76426
Harsh Sahare <harshsahare1212@gmail.com>
2025-09-05 16:47:35 +0530

    refactored: changed the navbar to work in export files

--- app/components/layout/navbar/menuItems.tsx
+++ app/components/layout/navbar/menuItems.tsx
@@ -1,23 +1,28 @@
 "use client";
-import React, { JSX } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 type MenuItemsProps = {
   name: string;
   link?: string;
 };
 
-export default function MenuItems({ name, link }: MenuItemsProps): JSX.Element {
-  const pathname = usePathname();
+export default function MenuItems({ name, link }: MenuItemsProps) {
+  const [pathname, setPathname] = useState<string>("/");
   const href = link || `/${name.toLowerCase()}`;
-  const isSelected = pathname === href;
+
+  useEffect(() => {
+    setPathname(window.location.pathname.split("/")[1]);
+    console.log(window.location.pathname, name);
+  }, []);
+
+  const isSelected = pathname.toLowerCase() === name.toLowerCase();
 
   return (
     <Link
       href={href}
       className={`uppercase cursor-pointer py-0.1 px-3 rounded-2xl hover:text-primary ${
-        isSelected ? "border-1  border-primary" : ""
+        isSelected ? "border-1 border-primary" : ""
       }`}
     >
       <span className="text-xl">{name.charAt(0)}</span>
